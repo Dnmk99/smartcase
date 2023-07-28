77d99fa7c37e4405dc6dcc8d6e1887599d377194
Dnmk99 <ti18.mika@gmail.com>
2023-07-29 01:13:55 +0200

    contact form

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,48 +1,35 @@
 // Function to create Intersection Observer for multiple elements
 function observeElements(elements) {
     elements.forEach((element) => {
-      let hasIntersected = false; // Flag to track if the element has intersected
-  
+      let hasIntersected = false;
       const observer = new IntersectionObserver(
         (entries, observer) => {
           entries.forEach((entry) => {
             // Check if the current element is in the viewport
             if (entry.isIntersecting && !hasIntersected) {
-              // If the element is in the viewport for the first time, add the "transition" and "active" classes
               element.classList.add('transition', 'active');
-              hasIntersected = true; // Set the flag to true, so the classes won't be toggled again
+              hasIntersected = true; 
             }
           });
         },
-        // Options for the Intersection Observer (in this case, we'll use the default options)
       );
-  
-      // Start observing the current element
       observer.observe(element);
     });
   }
-  
-  // Function to check if the user has scrolled to the bottom of the page
   function isScrolledToBottom() {
-    const scrollPosition = window.scrollY; // Current scroll position
-    const documentHeight = document.documentElement.scrollHeight; // Height of the entire document
-    const windowHeight = window.innerHeight; // Height of the visible window
-  
-    return scrollPosition + windowHeight >= documentHeight; // True if scrolled to the bottom, false otherwise
+    const scrollPosition = window.scrollY; 
+    const documentHeight = document.documentElement.scrollHeight; 
+    const windowHeight = window.innerHeight; 
+    return scrollPosition + windowHeight >= documentHeight; 
   }
-  
-  // Get all elements with the class "your-elements"
   const targetElements = document.querySelectorAll('.reveal');
-  
-  // Call the function and pass the target elements to observe
   observeElements(targetElements);
   
   // Event listener to trigger an action when the user scrolls to the bottom of the page
   window.addEventListener('scroll', () => {
     if (isScrolledToBottom()) {
-      // If scrolled to the bottom, do something
-      console.log('Scrolled to the bottom!');
-      // Add your logic here to perform an action when the user has scrolled to the bottom of the page
+     
     }
   });
+  
   
